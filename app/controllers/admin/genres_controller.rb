dbdf811d417b7c27751d21281badd6e40a3cfe9f
EC2 Default User <ec2-user@ip-172-31-81-179.ec2.internal>
2022-06-04 10:00:33 +0000

    [Add] 応用課題

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -10,9 +10,9 @@ class Admin::GenresController < ApplicationController
   end
   
   def create
-    genre = Genre.new
+    genre = Genre.new(genre_params)
     genre.save
-    redirect_to 'admin/genres/index'
+    redirect_to admin_genres_path
   end
 
   def edit
@@ -22,7 +22,7 @@ class Admin::GenresController < ApplicationController
   def update
     genre = Genre.find(params[:id])
     genre.update(genre_params)
-    redirect_to 'admin/genres/index'
+    redirect_to admin_genre_path
   end
   
     private
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
  
   namespace :admin do
-    get 'genres/index'
-    get 'genres/:id/edit' => 'genres#edit' , as:'edit_genre'
-    get 'genres/new'
-    post 'genres' => 'genres#create'
-    
+    get 'items/index'
+    get 'items/new'
+    get 'items/show'
+    get 'items/edit'
+    resources :genres
   end
+  
+  
   namespace :public do
     get 'homes/top'
   end
