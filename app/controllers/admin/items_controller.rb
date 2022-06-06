b571b4d8746c42a85bcc2c9949bf56b15d570018
EC2 Default User <ec2-user@ip-172-31-81-179.ec2.internal>
2022-06-06 00:32:12 +0000

    [Add] 応用課題

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -5,6 +5,7 @@ class Admin::ItemsController < ApplicationController
 
   def new
     @item = Item.new
+    @genres = Genre.all
   end
   
   def create
@@ -23,7 +24,7 @@ class Admin::ItemsController < ApplicationController
   private
   
   def item_params
-    params.require(:item).permit(:genre_id,:name,:introduction,:price)
+    params.require(:item).permit(:image,:genre_id,:name,:introduction,:price,:is_active)
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
  
   namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :admin do
     resources :items
     resources :genres
+    resources :customers
   end
   
   
