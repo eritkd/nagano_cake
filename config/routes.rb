bc4828afada237dfbe57ccc250f76860bfc6be7e
EC2 Default User <ec2-user@ip-172-31-81-179.ec2.internal>
2022-06-08 08:49:50 +0000

    [Add] 応用課題

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   
 
  
-  devise_for :customers,skip: [:passwords], controllers: {
+devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
