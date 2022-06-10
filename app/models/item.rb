643dd76cf95c0edfda30ec9d00a10b4cfa85e675
EC2 Default User <ec2-user@ip-172-31-81-179.ec2.internal>
2022-06-10 09:04:10 +0000

    [Add] 応用課題

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
      has_one_attached :image
      belongs_to :genre
+     enum item_method: { on_sale: true, stop_selling: false }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
  
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+  end
   namespace :admin do
     resources :items
     resources :genres
