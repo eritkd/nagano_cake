a8afff600de4f816acd8356d73668a0d25298b80
EC2 Default User <ec2-user@ip-172-31-81-179.ec2.internal>
2022-06-16 08:01:21 +0000

    [Add] 応用課題

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -14,4 +14,7 @@ class Admin::CustomersController < ApplicationController
   def customer_params
     params.require(:customer).permit(:last_name,:first_name,:last_name_kana,:first_name_kana,:postal_code,:address,:telephone_number,:email)
   end
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
 end
