62c8688437ed9c36bddab8e7cb2f78debc8d542f
EC2 Default User <ec2-user@ip-172-31-81-179.ec2.internal>
2022-06-27 02:36:56 +0000

    [Add] 応用課題

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -11,6 +11,12 @@ class Admin::CustomersController < ApplicationController
     @customer = Customer.find(params[:id]) 
   end
   
+  def update
+    customer = Customer.find(params[:id])
+    customer.update(customer_params)
+    redirect_to admin_customer_path(customer.id)
+  end
+  
   def unsubscribe
     @customer = Customer.find(params[:id])
   end
@@ -23,7 +29,7 @@ class Admin::CustomersController < ApplicationController
   end
   
   def customer_params
-    params.require(:customer).permit(:last_name,:first_name,:last_name_kana,:first_name_kana,:postal_code,:address,:telephone_number,:email)
+    params.require(:customer).permit(:last_name,:first_name,:last_name_kana,:first_name_kana,:postal_code,:address,:telephone_number,:email,:is_active)
   end
   def genre_params
     params.require(:genre).permit(:name)
