62d8a48b4bb7324173a76ffb428d9086fc6e6e45
EC2 Default User <ec2-user@ip-172-31-81-179.ec2.internal>
2022-06-27 02:58:38 +0000

    [Add]　応用課題

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -9,7 +9,7 @@ class Public::ItemsController < ApplicationController
   end
   
   def create
-    cart_item = Cart_item.new(cart_item_params)
+    cart_item = CartItem.new(cart_item_params)
     cart_item.save
     redirect_to public_item_path
   end
@@ -19,6 +19,7 @@ class Public::ItemsController < ApplicationController
   def cart_item_params
       params.require(:cart_item).permit(:item_id, :amount)
   end
+  
   def item_params
     params.require(:item).permit(:image,:genre_id,:name,:introduction,:price,:is_active)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_24_051803) do
+ActiveRecord::Schema.define(version: 2022_06_27_025725) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2022_06_24_051803) do
     t.integer "amount"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
   create_table "customers", force: :cascade do |t|
